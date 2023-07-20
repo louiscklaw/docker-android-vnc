2a3935aa48a5f8a21e159757201b41d93540fbba
Louis Law <louiscklaw@users.noreply.github.com>
2023-07-21 00:25:22 +0800

    update,

--- build.sh
+++ build.sh
@@ -34,13 +34,6 @@ cd -
       --build-arg="ANDROID_API_LEVEL=$ANDROID_API_LEVEL" \
       --build-arg="SCREEN_RESOLUTION=$SCREEN_RESOLUTION" \
       -t openbox-android
-
-    docker build . \
-      --build-arg="ANDROID_API_LEVEL=$ANDROID_API_LEVEL" \
-      --build-arg="SCREEN_RESOLUTION=$SCREEN_RESOLUTION" \
-      -t openbox-android
-
-
   cd -
 
 # finialize docker
@@ -50,16 +43,24 @@ cd dockerfiles/final
     -t openbox-android
 cd -
 
-docker image tag openbox-android 192.168.10.61:5000/logickee/openbox-android
+# cd dockerfiles/android
+#   docker build . \
+#     --build-arg="ANDROID_API_LEVEL=$ANDROID_API_LEVEL" \
+#     --build-arg="SCREEN_RESOLUTION=$SCREEN_RESOLUTION" \
+#     -t openbox-android
+# cd -
+
+# docker image tag openbox-android 192.168.10.61:5000/logickee/openbox-android
 
 # docker push 192.168.10.61:5000/logickee/openbox-android 
-docker run --rm -it \
-  --privileged \
-  --device /dev/kvm \
-  -v ./share:/share \
-  -v ./dockerfiles/final/etc/supervisord-emulator.conf:/etc/supervisord-emulator.conf \
-  -p 15900:5900 \
-  -p 6080:6080 \
-  -p 4723:4723 \
-  --name logickee_docker_android openbox-android 
-  # bash
+
+# docker run --rm -it \
+#   --privileged \
+#   --device /dev/kvm \
+#   -v ./share:/share \
+#   -v ./dockerfiles/final/etc/supervisord-emulator.conf:/etc/supervisord-emulator.conf \
+#   -p 15900:5900 \
+#   -p 6080:6080 \
+#   -p 4723:4723 \
+#   --name logickee_docker_android openbox-android 
+#   # bash
